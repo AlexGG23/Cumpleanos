1ad6711cba5e4201ded07a81d6d555295047b611
AlexGG23 <98983574+AlexGG23@users.noreply.github.com>
2024-10-07 11:26:09 -0600

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ var w = c.width = window.innerWidth,
 		hh = h / 2,
 		
 		opts = {
-			strings: [ 'FELIZ', 'CUMPLEAÑOS', ' AUXILAR', ' DE', ' RH', ' PAULINA', ' ROSALES' ],
+			strings: [ 'FELIZ', 'CUMPLEAÑOS', ' ENCARGADA', ' DE', ' ALMACEN', ' ARISDELCY', ' LORENZO' ],
 			charSize: 30,
 			charSpacing: 35,
 			lineHeight: 40,
